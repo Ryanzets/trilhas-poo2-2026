6fcc2243c384ac2caec256bd367969cc95434101
Ryanzets <ryanposcidonio@gmail.com>
2026-02-23 14:13:36 -0300

    Classe Principal Adicionada

---
+++ src/main/java/br/edu/tds/projetopoo/Cliente.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.tds.projetopoo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente{
+    private String nome;
+    private int idade;
+    private String cpf;
+    
+    
+    public Cliente(){
+        
+    }
+    
+    public Cliente (String n, int i, String cpf){
+        this.nome = n;
+        this.idade = i;
+        this.cpf = cpf;
+    }
+    
+    public void imprimeDadosCliente(){
+        System.out.println("Nome.: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("CPF..: " + this.cpf);
+    }
+}
